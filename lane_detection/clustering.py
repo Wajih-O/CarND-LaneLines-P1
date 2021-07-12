1d703f2956cfce311e95554c801c8e90a90863b8
Wajih Ouertani <wajih.ouertani@gmail.com>
2021-07-12 10:26:22 +0200

    update utils and move clustering classes and helpers to clustering.py

---
+++ lane_detection/clustering.py
@@ -0,0 +1,235 @@
+from typing import List, Tuple, Optional
+import math
+import numpy as np
+from itertools import product
+from queue import PriorityQueue
+from abc import abstractmethod
+
+from itertools import chain
+
+from pprint import pprint
+import logging
+
+from lane_detection.segment import Segment
+
+# Configure logging should be moved
+logging.basicConfig(filename='utils.log', level=logging.DEBUG)
+
+
+
+
+class Mergeable:
+    """ A helper class to contain merged nodes indexed by the sorted tuple of original indices"""
+    def __init__(self, id_:Tuple[int]):
+        self._id = id_ # tuple(sorted(list(id_))) # tuple merged  (the order matters)
+
+    def __repr__(self):
+        return str(self._id)
+
+    def __hash__(self):
+        return int(''.join(map(str,sorted(self._id))))
+
+    def cache(self, store:dict):
+        """ Compute the merge and store it """
+
+    def retrieve(self, store) -> Optional["Mergeable"]:
+        """Retrieves the merge from a store"""
+        return store.get(self._id, None)
+
+    @abstractmethod
+    def compute(self, cache: Optional[dict]=None, update=False):
+        """ Compute the merge using optionally a store/cache"""
+
+
+class SegmentCluster(Mergeable):
+    def __init__(self, id_:Tuple[int], data=List[Segment]):
+        super().__init__(id_)
+        self.__data = data
+        self._logger = logging.getLogger(__name__) # init logger
+
+
+    def compute(self, cache: Optional[dict]=None, update=False):
+        """ Compute a segment merge from original container and a cache
+            :cache: a cache/storing precomputed merge
+            :param update: update the cache with the newly computed items.
+        """
+        self._logger.debug(self._id)
+        if cache and self._id in cache:
+            return cache[self._id]
+
+        # merge
+        if len(self._id) == 1:
+            return self.__data[self._id[0]]
+        output = SegmentCluster(self._id[:-1], data=self.__data).compute(cache, update=update).merge(self.__data[self._id[-1]])
+        if update:
+            cache[self._id] = output
+        return output
+
+    def sim(self, other: "SegmentCluster", similarity="cosine", merge=True, linkage=np.min, cache:Optional[dict]={}):
+        """ A helper to compute similarity between two Segment clusters
+            using element-wise-similarity/or merged
+            :param other: The segment cluster with which we want compute the similarity
+            :param merge: Merge the cluster to one segment before
+            :param linkage: The type of aggregation of individual similarity.
+            :param cache:
+        """
+        if merge:
+            # Merge/interpolate/average the segment cluster into one segment before applying the similarity
+            return self.compute().sim(other.compute(),similarity=similarity)
+
+        else:
+            # TODO: (optionally) use the linkage function to compute similarity between the two cluster instead of the merge/extended segment
+            pass
+
+
+class SimContainer:
+    """ A helper class that contains similar items for fast retrieval"""
+    def __init__(self, item):
+        self.__item = item
+        self.__queue = PriorityQueue()
+        self.__best = None
+
+        self._similar = set() # keeps track of similar items
+        self._removed = set() # keeps track of  the removed keys/items
+
+    def __repr__(self):
+        repr_str = ''
+        if self.__best:
+            repr_str += str(self.__best)
+        repr_str += str(self.__queue.queue)
+        return repr_str
+
+    def put(self, similar, similarity):
+        """ Put a similar item in the container
+        :param similar: a similar item (hashable) or a key
+        :param similarity: a non zero similarity to the container self._item
+        """
+        if similar in self._removed:
+            self._removed.remove(similar)
+        self._similar.add(similar)
+
+        self.__queue.put((1/similarity, similar, similarity))
+        if self.__best:
+            self.__queue.put(self.__best) # put back the best item to compete with the new one
+
+        self.__best = self.__queue.get() # caching the best similar item (according to similarity)
+        while self.__best[1] in self._removed: # the best is not valid anymore as it was removed
+            self.__best = self.__queue.get()
+
+    def remove(self, key):
+        """ A helper to remove an item from the container (only by key)"""
+        if key in self._similar:
+            # the item exist -> update the queue
+            self._similar.remove(key)
+            self._removed.add(key)
+            while self.__best and self.__best[1] in self._removed:
+                # update the best
+                if not self.__queue.empty():
+                    self.__best = self.__queue.get()
+                else:
+                    self.__best = None
+
+    def best(self):
+        """ return the best similarity in this container"""
+        return self.__best
+
+    @property
+    def queue(self):
+        """ return the queue. """
+        return self.__queue.queue
+    @property
+    def similar(self):
+        similar_items = []
+        if self.__best is None:
+            return []
+        else:
+            similar_items.append(self.__best[1]) # update the similar items with the best one (cached)
+        if len(self.__queue.queue):
+            similar_items.extend([item[1] for item in self.__queue.queue if item[1] not in self._removed])
+        return similar_items
+
+
+def agglomerate(data: List[Segment], threshold: float, similarity="cosine") -> Tuple[dict, dict]:
+    """ Agglomerate data item given
+        using agglomerative-clustering (grouping the closest cluster first)
+
+    (todo: simultaneous grouping before updating the)
+
+    :param threshold: similarity threshold above which 2 items
+                     are considered similar and could be merged
+    :param similarity: The similarity measure between 2 segments
+
+    :return: Tuple/Couple of clusters' dictionary where the keys are the cluster identified by the index of the item in the original data
+    the `data` parameter and the values are the similar item non yet merged/will not because the similarity with these items is below the
+    `threshold` parameter. And the store of computed merged segment of the cluster and the intermediate merges that lead to the final
+    returned clusters.
+    """
+    logger = logging.getLogger(__name__)
+    def get_merge_candidates(sim_dict):
+        """ a helper to get candidates to merge"""
+        pq_ = PriorityQueue()
+        for key, value in sim_dict.items():
+            best = value.best() # get best item from the SimContainer (value)
+            if best:
+                pq_.put((best, key))
+
+        if  pq_.empty():
+            return None, None
+        else:
+            return pq_.get()
+
+
+    # Build the similarity between
+    data_dict = {} # a dictionary
+    for index, item in  enumerate(data):
+        data_dict[(index,)] = item
+
+    # Sorted segment by length (from the longest to the shortest segment)
+    # to prioritize the segments to be merged (short or long ones)
+    segments = sorted([(key, segment) for key, segment in data_dict.items()], key=lambda x: x[1].norm, reverse=True)
+
+    # populate similarities (Matrix as a dictionary)
+    similarities = {}
+    for i in range(len(segments)):
+        # similarities.
+        key_i, segment_i = segments[i]
+        similarities[key_i] = SimContainer(segment_i)
+        for  j in range(1, len(segments)):
+            key_j, segment_j = segments[j]
+            similarities[key_i].put(key_j, segment_i.sim(segment_j, similarity=similarity))
+
+    # Search for the best items to agglomerate/merge
+    best, orig  = get_merge_candidates(similarities)
+    while best and (best[2] > threshold):
+        to_merge = {orig, best[1]}
+        logger.debug(f"  Merging (clusters) -> {to_merge}")
+        merged = SegmentCluster(orig+best[1], data) # Merged cluster
+        merged_segment = merged.compute(cache=data_dict, update=True)
+
+        # Similar candidates to the cluster to update their similarity entries (previous similar to the (orig and best))
+
+        to_update = set(chain(*[container.similar for container in
+            filter(lambda item: item is not None, [similarities.get(key, None) for key in  to_merge])]))
+
+        # remove the merged part
+        for key in to_merge:
+            if key in similarities:
+                del similarities[key]
+
+        # remove the merged item from the other similarity container
+        for key, sim_container in similarities.items():
+            sim_container.remove(orig)
+            sim_container.remove(best[1])
+
+        to_update.difference_update({orig, best[1]}) # clean to_update from the already merged items
+        to_update.update(similarities.keys())
+
+        similarities[merged._id] = SimContainer(merged_segment)
+
+        for key in to_update:
+            similarities[merged._id].put(key, merged_segment.sim(data_dict[key], similarity=similarity))
+        # get the next best candidates to agglomerate
+        best, orig  =  get_merge_candidates(similarities)
+
+
+    return similarities, data_dict
--- lane_detection/utils.py
+++ lane_detection/utils.py
@@ -1,245 +1,153 @@
-from typing import List, Tuple, Optional
+from typing import List, Tuple, Optional, Dict
 import math
 import numpy as np
 from itertools import product
 from queue import PriorityQueue
 from abc import abstractmethod
 
-from itertools import chain
+from itertools import chain, combinations
 
 from pprint import pprint
 import logging
 
 import cv2
-from lane_detection.segment import Segment
+
+from lane_detection import Segment, Point
+from lane_detection.hough_lines import HoughLines
+from lane_detection.clustering import agglomerate
+from lane_detection.visu_utils import draw_lane
+
 
 # Configure logging should be moved
 logging.basicConfig(filename='utils.log', level=logging.DEBUG)
 
 
+def grayscale(img):
+    """Applies the Grayscale transform
+    This will return an image with only one color channel
+    but NOTE: to see the returned image as grayscale
+    (assuming your grayscaled image is called 'gray')
+    you should call plt.imshow(gray, cmap='gray')"""
+
+    return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # Or use BGR2GRAY if you read an image with cv2.imread()
+    # return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+def hsv_value(img):
+    """" Convert img to HSV and extract the value channel"""
+    return cv2.cvtColor(img, cv2.COLOR_BGR2HSV)[:, :, 2]
+
 
 def canny(gl_image, low_threshold=50, high_threshold=150):
-    """Applies the Canny transform"""
+    """ Applies the Canny transform"""
     return cv2.Canny(gl_image, low_threshold, high_threshold)
 
 def gaussian_blur(gl_image, kernel_size=5):
-    """Applies a Gaussian Noise kernel"""
+    """ Applies a Gaussian Noise kernel"""
     return cv2.GaussianBlur(gl_image, (kernel_size, kernel_size), 0)
 
 
-
-class Mergeable:
-    """ A helper class to contain merged nodes indexed by the sorted tuple of original indices"""
-    def __init__(self, id_:Tuple[int]):
-        self._id = id_ # tuple(sorted(list(id_))) # tuple merged  (the order matters)
-
-    def __repr__(self):
-        return str(self._id)
-
-    def __hash__(self):
-        return int(''.join(map(str,sorted(self._id))))
-
-    def cache(self, store:dict):
-        """ Compute the merge and store it """
-
-    def retrieve(self, store) -> Optional["Mergeable"]:
-        """Retrieves the merge from a store"""
-        return store.get(self._id, None)
-
-    @abstractmethod
-    def compute(self, cache: Optional[dict]=None, update=False):
-        """ Compute the merge using optionally a store/cache"""
+def horizon_crop(image: np.ndarray, horizon:Optional[int]=None) -> np.ndarray:
+    """ A helper to remove part of image above horizon line """
+    output_image = image.copy()
+    if horizon is None:
+        # assume the camera plan is normal to the road (the camera not tilted)
+        output_image[:image.shape[0]//2, :] = 0 # to keep it compatible with color image
+    return output_image
 
 
-class SegmentCluster(Mergeable):
-    def __init__(self, id_:Tuple[int], data=List[Segment]):
-        super().__init__(id_)
-        self.__data = data
-        self._logger = logging.getLogger(__name__) # init logger
-
-
-    def compute(self, cache: Optional[dict]=None, update=False):
-        """ Compute a segment merge from original container and a cache
-            :cache: a cache/storing precomputed merge
-            :param update: update the cache with the newly computed items.
-        """
-        self._logger.debug(self._id)
-        if cache and self._id in cache:
-            return cache[self._id]
-
-        # merge
-        if len(self._id) == 1:
-            return self.__data[self._id[0]]
-        output = SegmentCluster(self._id[:-1], data=self.__data).compute(cache, update=update).merge(self.__data[self._id[-1]])
-        if update:
-            cache[self._id] = output
-        return output
-
-    def sim(self, other: "SegmentCluster", similarity="cosine", merge=True, linkage=np.min, cache:Optional[dict]={}):
-        """ A helper to compute similarity between two Segment clusters
-            using element-wise-similarity/or merged
-            :param other: The segment cluster with which we want compute the similarity
-            :param merge: Merge the cluster to one segment before
-            :param linkage: The type of aggregation of individual similarity.
-            :param cache:
-        """
-        if merge:
-            # Merge/interpolate/average the segment cluster into one segment before applying the similarity
-            return self.compute().sim(other.compute(),similarity=similarity)
-
-        else:
-            # TODO: (optionally) use the linkage function to compute similarity between the two cluster instead of the merge/extended segment
-            pass
-
-
-class SimContainer:
-    """ A helper class that contains similar items for fast retrieval"""
-    def __init__(self, item):
-        self.__item = item
-        self.__queue = PriorityQueue()
-        self.__best = None
-
-        self._similar = set() # keeps track of similar items
-        self._removed = set() # keeps track of  the removed keys/items
-
-    def __repr__(self):
-        repr_str = ''
-        if self.__best:
-            repr_str += str(self.__best)
-        repr_str += str(self.__queue.queue)
-        return repr_str
-
-    def put(self, similar, similarity):
-        """ Put a similar item in the container
-        :param similar: a similar item (hashable) or a key
-        :param similarity: a non zero similarity to the container self._item
-        """
-        if similar in self._removed:
-            self._removed.remove(similar)
-        self._similar.add(similar)
-
-        self.__queue.put((1/similarity, similar, similarity))
-        if self.__best:
-            self.__queue.put(self.__best) # put back the best item to compete with the new one
-
-        self.__best = self.__queue.get() # caching the best similar item (according to similarity)
-        while self.__best[1] in self._removed: # the best is not valid anymore as it was removed
-            self.__best = self.__queue.get()
-
-    def remove(self, key):
-        """ A helper to remove an item from the container (only by key)"""
-        if key in self._similar:
-            # the item exist -> update the queue
-            self._similar.remove(key)
-            self._removed.add(key)
-            while self.__best and self.__best[1] in self._removed:
-                # update the best
-                if not self.__queue.empty():
-                    self.__best = self.__queue.get()
-                else:
-                    self.__best = None
-
-    def best(self):
-        """ return the best similarity in this container"""
-        return self.__best
-
-    @property
-    def queue(self):
-        """ return the queue. """
-        return self.__queue.queue
-    @property
-    def similar(self):
-        similar_items = []
-        if self.__best is None:
-            return []
-        else:
-            similar_items.append(self.__best[1]) # update the similar items with the best one (cached)
-        if len(self.__queue.queue):
-            similar_items.extend([item[1] for item in self.__queue.queue if item[1] not in self._removed])
-        return similar_items
-
-
-def agglomerate(data: List[Segment], threshold: float, similarity="cosine") -> Tuple[dict, dict]:
-    """ Agglomerate data item given
-        using agglomerative-clustering (grouping the closest cluster first)
-
-    (todo: simultaneous grouping before updating the)
-
-    :param threshold: similarity threshold above which 2 items
-                     are considered similar and could be merged
-    :param similarity: The similarity measure between 2 segments
-
-    :return: Tuple/Couple of clusters' dictionary where the keys are the cluster identified by the index of the item in the original data
-    the `data` parameter and the values are the similar item non yet merged/will not because the similarity with these items is below the
-    `threshold` parameter. And the store of computed merged segment of the cluster and the intermediate merges that lead to the final
-    returned clusters.
+def weighted_img(img, initial_img, α=0.8, β=1., γ=0.):
     """
-    logger = logging.getLogger(__name__)
-    def get_merge_candidates(sim_dict):
-        """ a helper to get candidates to merge"""
-        pq_ = PriorityQueue()
-        for key, value in sim_dict.items():
-            best = value.best() # get best item from the SimContainer (value)
-            if best:
-                pq_.put((best, key))
+    `img` is the output of the hough_lines(), An image with lines drawn on it.
+    Should be a blank image (all black) with lines drawn on it.
 
-        if  pq_.empty():
-            return None, None
-        else:
-            return pq_.get()
+    `initial_img` should be the image before any processing.
 
+    The result image is computed as follows:
 
-    # Build the similarity between
-    data_dict = {} # a dictionary
-    for index, item in  enumerate(data):
-        data_dict[(index,)] = item
+    initial_img * α + img * β + γ
 
-    # Sorted segment by length (from the longest to the shortest segment)
-    # to prioritize the segments to be merged (short or long ones)
-    segments = sorted([(key, segment) for key, segment in data_dict.items()], key=lambda x: x[1].norm, reverse=True)
+    """
+    return cv2.addWeighted(initial_img, α, img, β, γ)
 
-    # populate similarities (Matrix as a dictionary)
-    similarities = {}
-    for i in range(len(segments)):
-        # similarities.
-        key_i, segment_i = segments[i]
-        similarities[key_i] = SimContainer(segment_i)
-        for  j in range(1, len(segments)):
-            key_j, segment_j = segments[j]
-            similarities[key_i].put(key_j, segment_i.sim(segment_j, similarity=similarity))
+def interest_region_crop(image):
+    """ A helper masking the image with Mask with region of interest"""
+    if len(image.shape) == 3:
+        # multi-channel image (color)
+        height, width, _ = image.shape
+    else:
+        height, width = image.shape
 
-    # Search for the best items to agglomerate/merge
-    best, orig  = get_merge_candidates(similarities)
-    while best and (best[2] > threshold):
-        to_merge = {orig, best[1]}
-        logger.debug(f"  Merging (clusters) -> {to_merge}")
-        merged = SegmentCluster(orig+best[1], data) # Merged cluster
-        merged_segment = merged.compute(cache=data_dict, update=True)
+    left_bottom = [0, height-1]
+    right_bottom = [width, height-1]
+    apex = [width//2, int(height/1.7)]
 
-        # Similar candidates to the cluster to update their similarity entries (previous similar to the (orig and best))
-
-        to_update = set(chain(*[container.similar for container in
-            filter(lambda item: item is not None, [similarities.get(key, None) for key in  to_merge])]))
-
-        # remove the merged part
-        for key in to_merge:
-            if key in similarities:
-                del similarities[key]
-
-        # remove the merged item from the other similarity container
-        for key, sim_container in similarities.items():
-            sim_container.remove(orig)
-            sim_container.remove(best[1])
-
-        to_update.difference_update({orig, best[1]}) # clean to_update from the already merged items
-        to_update.update(similarities.keys())
-
-        similarities[merged._id] = SimContainer(merged_segment)
-
-        for key in to_update:
-            similarities[merged._id].put(key, merged_segment.sim(data_dict[key], similarity=similarity))
-        # get the next best candidates to agglomerate
-        best, orig  =  get_merge_candidates(similarities)
+    interest_region_polygon = np.array([list(map(lambda xy: (xy[0] , xy[1]), [left_bottom, apex, right_bottom]))])
+    masked_edges = np.copy(image)
+    inv_mask = np.copy(image)
+    cv2.fillPoly(inv_mask, pts=interest_region_polygon, color = (0))
+    return masked_edges - inv_mask
 
 
-    return similarities, data_dict
+# for slope Similarity it is a exp kernel on absolute difference with configurable sigma (for the kernel)
+config = {"threshold": 0.95, "similarity": "slope", "slope_threshold": 0.5}
+
+
+def extract_lane(image, config=config) -> List[Segment]:
+    """ A helper to extract the  2 segments defining the lane """
+    # Detect edge using canny on a gaussian blur
+    if len(image.shape) not in {2, 3}:
+        raise Exception("Not supported dimension")
+    if len(image.shape) == 3:
+        width, height, _ = image.shape
+    else:
+        assert len(image.shape) == 2
+        width, height = image.shape
+    logging.debug(f"image dimension {width}x{height}")
+    edges_image = interest_region_crop(horizon_crop(canny(gaussian_blur(grayscale(image)))))
+    # Extract segments (detect lines) then filter the horizontal and nearly horizontal ones
+    segments = list(filter(lambda segment: not segment.nearly_vertical(.2) and not segment.nearly_horizontal(config.get("slope_threshold", 0.5)), [Segment(item) for item in np.squeeze(HoughLines()(edges_image))]))
+    # Iterative segments merge (agglomerative approach with shrinking threshold)
+
+    threshold = config.get("threshold")
+    while len(segments) > 2: # iterative adaptation of the threshold
+        cluster_dict, segments_store = agglomerate(segments, threshold=threshold, similarity=config.get("similarity"))
+        # Filter/Sort and Select the 2 longest (merged/extended line) segments as the candidate for lane detection
+        merged_segments = sorted(filter(lambda segment: segment is not None, [segments_store.get(key, None) for key in cluster_dict]), key=lambda segment: segment.length, reverse=True)
+        segments =  merged_segments[:]
+        threshold *= .95 #  more tolerance to the similarity threshold
+
+    right, left = None, None
+    left_right =  sorted(sorted(filter(lambda segment: segment.length > 40, segments), key=lambda segment: segment.length, reverse=True), key=lambda segment: segment.slope)[:2]
+    if len(left_right) == 2:
+        left, right = left_right
+        # extending the right and left side with the projection of the center of the image
+        # expectedly close to the horizon (to replace with the intersection of the 2 lane lines/sides)
+        height, width = image.shape[:2]
+        # center = Point(width//2, height//2)
+        # Extending the right and left side segments of the lane to their intersection
+        intersection = right.intersection(left)
+        right = right.point_extend(intersection, inclusive=False)
+        left = left.point_extend(intersection, inclusive=False)
+        # Extending the right and left side segments of the lane to the bottom of the image
+        right = right.point_extend(right.intersection(Segment((0, height-1, width-1, height-1))), inclusive=False)
+        left = left.point_extend(left.intersection(Segment((0, height-1, width-1, height-1))), inclusive=False)
+
+
+    # preparing output dict with left and right annotation as keys
+    output_dict = {}
+    if left:
+        output_dict["left"] = left
+    if right:
+        output_dict["right"] = right
+
+    return output_dict
+
+
+def save_image(image,  output_path: str):
+    """ Save/Write image to output_path """
+
+def find_lane(image) -> List[Segment]:
+    extraction_output = extract_lane(image)
+    draw_lane(image, extraction_output)
+    return extraction_output
